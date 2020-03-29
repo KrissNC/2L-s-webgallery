efe1be0a43b41591cb26acfdcdd3d0c2a9541546
KrissNC <mistralkriss@gmail.com>
2020-03-29 23:20:00 +0200

    adding build for deploiment

---
+++ client/build/precache-manifest.f12926856477130068803588d1f1e845.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d75853bdecc5272b51651d68c4b4272d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "700936413b85358045ec",
+    "url": "/static/css/main.542ad988.chunk.css"
+  },
+  {
+    "revision": "4759110446cbaa61477b",
+    "url": "/static/js/2.95757592.chunk.js"
+  },
+  {
+    "revision": "700936413b85358045ec",
+    "url": "/static/js/main.e58ca7d5.chunk.js"
+  },
+  {
+    "revision": "331fbab2d8ffe9bfc42f",
+    "url": "/static/js/runtime-main.3f6782cb.js"
+  }
+]);
